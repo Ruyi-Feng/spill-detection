5d3d353c40e380d01c3c0f3f285ecc05d586a4bc
Dongyu Tian <2502533535@qq.com>
2023-12-05 18:31:39 +0800

    feat: write the function of recieving and sending

---
+++ msg_driver/receive/receive.py
@@ -0,0 +1,6 @@
+def recieve(msg):
+    '''
+    接受传来的数据message, 将原始数据格式转化为代码内流通的数据格式。
+    '''
+    newMsg = msg    # 默认情况下，直接接受不做处理
+    return newMsg
---
+++ msg_driver/send/send.py
@@ -0,0 +1,6 @@
+def send(msg):
+    '''
+    将代码内部流通的数据, 转化为输出需要的格式。
+    '''
+    newMsg = msg    # 默认情况下，直接接受不做处理
+    return newMsg
