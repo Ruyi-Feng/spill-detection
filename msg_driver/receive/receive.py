4da1f3047530c9fcf245dae76cdedc9f210e8e59
Dongyu Tian <2502533535@qq.com>
2023-12-06 22:59:54 +0800

    feat: process "a" property in recieve and send

--- msg_driver/receive/receive.py
+++ msg_driver/receive/receive.py
@@ -1,6 +1,15 @@
 def recieve(msg):
     '''
     接受传来的数据message, 将原始数据格式转化为代码内流通的数据格式。
+    input
+    ------
+    msg: list, 传感器数据.
+    msg元素为代表一个车辆目标的dict
+    
+    func
+    ------
+    1. 为每个target目标增加加速度属性a
     '''
-    newMsg = msg    # 默认情况下，直接接受不做处理
-    return newMsg
+    for i in range(len(msg)):
+        msg[i]['a'] = 0
+    return msg
--- msg_driver/send/send.py
+++ msg_driver/send/send.py
@@ -1,6 +1,15 @@
 def send(msg):
     '''
     将代码内部流通的数据, 转化为输出需要的格式。
+    input
+    ------
+    msg: list, 内部流通数据.
+    msg元素为代表一个车辆目标的dict
+    
+    func
+    ------
+    1. 为每个target目标删除加速度属性a
     '''
-    newMsg = msg    # 默认情况下，直接接受不做处理
-    return newMsg
+    for i in range(len(msg)):
+        del msg[i]['a']
+    return msg
