f77258f87aaac8d45b3740841e8951ff01ee11a8
Dongyu Tian <2502533535@qq.com>
2023-12-06 23:27:16 +0800

    fix: update the function notes in dataloader

--- rsu_simulator/dataloader.py
+++ rsu_simulator/dataloader.py
@@ -1,11 +1,19 @@
 # -*-coding=utf-8-*-
 import json
 
-def loadDataByLine(path):
-    '''
-    按行加载读取轨迹txt数据
-    txt数据格式：
-    每行为一帧的目标，形式为list，其中元素为代表目标信息的dict。
+def loadDataByLine(path: str) -> None:
+    '''function loadDataByLine
+
+    input
+    ------
+    path: str
+        txt文件路径。txt文件按行存有msg, msg为list格式, list元素为代表车辆目标的dict。
+
+    return
+    ------
+    None
+
+    按行加载读取轨迹txt数据, 可打印输出。
     '''
     with open(path) as f:
         for line in f.readlines():
@@ -16,12 +24,20 @@ def loadDataByLine(path):
                 pass
 
 
-def loadDataAsDict(path):
-    '''
-    加载txt数据，并保存到一个dict中.
-    txt数据格式：
-    每行为一帧的目标，形式为list，其中元素为代表目标信息的dict。
-    返回的组织格式{frame:[]target}。
+def loadDataAsDict(path: str) -> dict:
+    '''function loadDataAsDict
+    
+    input
+    ------
+    path: str
+        txt文件路径。txt文件按行存有msg, msg为list格式, list元素为代表车辆目标的dict。
+
+    return
+    ------
+    allData: dict
+        以帧为索引的dict, 每帧的数据为list, list元素为代表车辆目标的dict。
+
+    从txt文件中加载msg, 以{frame:[]target}的组织格式返回。
     '''
     # 存储到整体dict，帧为索引
     allData = dict()
@@ -38,12 +54,20 @@ def loadDataAsDict(path):
     print("总帧数", frame+1)
     return allData
 
-def loadDataAsList(path):
-    '''
-    加载txt数据，并保存到一个list中.
-    txt数据格式：
-    每行为一帧的目标，形式为list，其中元素为代表目标信息的dict。
-    返回的组织格式[[]target, []target, []target, ...]。
+def loadDataAsList(path: str) -> list:
+    '''function loadDataAsList
+    
+    input
+    ------
+    path: str
+        txt文件路径。txt文件按行存有msg, msg为list格式, list元素为代表车辆目标的dict。
+
+    return
+    ------
+    allData: list
+        每帧的数据为list, list元素为代表车辆目标的dict。
+
+    从txt文件中加载msg, 以[[]target, []target, []target, ...]的组织格式返回。
     '''
     # 存储到整体dict，帧为索引
     allData = []
