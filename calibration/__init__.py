b2312745686b7dea88a51c5e5c635ebe7af34d1d
Dongyu Tian <2502533535@qq.com>
2023-12-06 22:58:31 +0800

    fix: move Calibrator to __init__.py

---
+++ calibration/__init__.py
@@ -0,0 +1,24 @@
+import json
+
+class Calibrator():
+    '''
+    生成标定器，用于标定检测区域的有效行驶片区和应急车道。
+    '''
+    def __init__(self):
+        self.trafficTarget = {}
+        self.laneTraffic = {}
+        self.calibration = {}
+        
+
+    def recieve(self, msg):
+        '''
+        接受每帧传输来的目标信息，更新给calibrator
+        '''
+        self.trafficTarget[msg['frame']] = msg['target']    # TODO1
+    
+    def calibrate(self):
+        self.calibration = {}
+
+    def save(self, path):
+        with open(path, 'w') as f:
+            json.dump(self.calibration, f)
