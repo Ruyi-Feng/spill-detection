5eb93c850e462826d09b0c0eb5b277ed9977fca0
Dongyu Tian <2502533535@qq.com>
2023-12-07 16:48:02 +0800

    feat: update receive and send driver for speed

--- msg_driver/__init__.py
+++ msg_driver/__init__.py
@@ -1,3 +1,4 @@
+import math 
 def receive(msg: list) -> list:
     '''function receive
     
@@ -10,10 +11,12 @@ def receive(msg: list) -> list:
     msg: list, 代码内流通的数据格式。
 
     接受传来的数据message, 将原始数据格式转化为代码内流通的数据格式。返回值与接受的msg相比: 
-    1. 为每个target目标增加加速度属性a
+    1. 增加加速度属性a
+    2. 增加速度属性speed, 单位m/s
     '''
     for i in range(len(msg)):
         msg[i]['a'] = 0
+        msg[i]['speed'] = math.sqrt(msg[i]['VDecVx']**2 + msg[i]['VDecVy']**2)
     return msg
 
 def send(msg: list) -> list:
@@ -28,8 +31,10 @@ def send(msg: list) -> list:
     msg: list, 输出到外部的数据。
 
     将代码内部流通的数据, 转化为输出需要的格式。返回值与代码内流通相比相比: 
-    1. 为每个target目标删除加速度属性a
+    1. 删除加速度属性a
+    2. 删除速度属性speed
     '''
     for i in range(len(msg)):
         del msg[i]['a']
+        del msg[i]['speed']
     return msg
