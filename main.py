9a9ddf60ec99736956f4334c33996ec85d48b620
Dongyu Tian <2502533535@qq.com>
2023-12-07 21:15:43 +0800

    fix: update main

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
+import json
 from controller import Controller
 
 if __name__ == "__main__":
-    configPath = './param.json'
-    calibrationPath = './calibration/config.json'
+    configPath = './config.json'
+    clbPath = './calibration/clb.json'
     dataPath = './data/result.txt'
     
     # 应在主代码开头生成控制器
-    controller = Controller(configPath, calibrationPath, dataPath)
+    controller = Controller(configPath, clbPath)
 
     # 模拟接受数据
     with open(dataPath) as f:
-        for data in f.readlines():
+        for msg in f.readlines():
             # 接受数据
-            msg, traffic, event = controller.receive(data)  # msg为控制器返回的需要发送的数据
+            try:
+                msg = json.loads(msg) # 接收到list数据
+            except:
+                pass    # 非检测信息则会接收到str数据
+    
+            msg, traffic, event = controller.receive(msg)  # msg为控制器返回的需要发送的数据
+            # print(msg)
 
