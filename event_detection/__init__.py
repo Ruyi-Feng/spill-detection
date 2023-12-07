39a1d83e38d141a4bd6cdd7970d5339cc17f6275
Dongyu Tian <2502533535@qq.com>
2023-12-07 21:13:56 +0800

    fix: move event funcs to init

--- event_detection/__init__.py
+++ event_detection/__init__.py
@@ -1,49 +1,220 @@
-from detection import *
 default_event_types = ['crowd', 'high_speed', 'illegal_occupation', 'incident_single_car', 
                       'incident', 'intensive_speed_reduction', 'low_speed', 'spill']
 
-def event_detection(msg, traffic, config, param, event_types = default_event_types):
+class EventDetector:
+    def __init__(self, fps, clb, event_types = default_event_types):
+        self.config = config
+        self.clb = clb
+        self.types = event_types
+        
+    def run(self, msg, traffic):
+        '''
+        检测交通事件，输出并返回事件列表
+        input
+        ------
+        msg: list, 传感器数据
+        traffic: dict, 交通流数据
+        config: dict, 标定参数
+        clb: dict, 算法参数
+        event_types: list, 需要检测的事件列表。
+
+        output
+        ------
+        events: list, 事件列表, 元素为event的衍生类
+        '''
+        # 事件检测
+        events = []
+        if 'crowd' in self.types:
+            events_c = crowdDetect(msg, traffic, self.config, self.clb)
+            events += events_c
+        if 'high_speed' in self.types:
+            events_h = highSpeedDetect(msg, traffic, self.config, self.clb)
+            events += events_h
+        if 'illegal_occupation' in self.types:
+            events_o = illegalOccupationDetect(msg, traffic, self.config, self.clb)
+            events += events_o
+        if 'incident_single_car' in self.types:
+            events_is = incidentSingleCarDetect(msg, traffic, self.config, self.clb)
+            events += events_is
+        if 'incident' in self.types:
+            events_i = incidentDetect(msg, traffic, self.config, self.clb)
+            events += events_i
+        if 'intensive_speed_reduction' in self.types:
+            events_r = intensiveSpeedReductionDetect(msg, traffic, self.config, self.clb)
+            events += events_r
+        if 'low_speed' in self.types:
+            events_l = lowSpeedDetect(msg, traffic, self.config, self.clb)
+            events += events_l
+        if 'spill' in self.types:
+            events_s = spillDetect(msg, traffic, self.config, self.clb)
+            events += events_s
+        
+        return events
+
+
+def crowdDetect(msg, traffic, config, clb):
     '''
-    检测交通事件，输出并返回事件列表
+    检测拥堵事件，输出并返回事件列表
     input
     ------
     msg: list, 传感器数据
     traffic: dict, 交通流数据
     config: dict, 标定参数
-    param: dict, 算法参数
-    event_types: list, 需要检测的事件列表。
+    self.clb: dict, 算法参数
 
     output
     ------
     events: list, 事件列表, 元素为event的衍生类
     '''
-    # 事件检测
-    events = []
-    if 'crowd' in event_types:
-        events_c = crowd_detect(msg, traffic, config, param)
-        events += events_c
-    if 'high_speed' in event_types:
-        events_h = high_speed_detect(msg, traffic, config, param)
-        events += events_h
-    if 'illegal_occupation' in event_types:
-        events_o = illegal_occupation_detect(msg, traffic, config, param)
-        events += events_o
-    if 'incident_single_car' in event_types:
-        events_is = incident_single_car_detect(msg, traffic, config, param)
-        events += events_is
-    if 'incident' in event_types:
-        events_i = incident_detect(msg, traffic, config, param)
-        events += events_i
-    if 'intensive_speed_reduction' in event_types:
-        events_r = intensive_speed_reduction_detect(msg, traffic, config, param)
-        events += events_r
-    if 'low_speed' in event_types:
-        events_l = low_speed_detect(msg, traffic, config, param)
-        events += events_l
-    if 'spill' in event_types:
-        events_s = spill_detect(msg, traffic, config, param)
-        events += events_s
-    
-    return events
+    events_c = []
 
-    
+    return events_c
+
+
+def highSpeedDetect(msg, traffic, config, clb):
+    '''
+    检测拥堵事件，输出并返回事件列表
+    input
+    ------
+    msg: list, 传感器数据
+    traffic: dict, 交通流数据
+    config: dict, 标定参数
+    self.clb: dict, 算法参数
+
+    output
+    ------
+    events: list, 事件列表, 元素为event的衍生类
+    '''
+    events_h = []
+
+    return events_h
+
+
+def illegalOccupationDetect(msg, traffic, config, clb):
+    '''
+    检测拥堵事件，输出并返回事件列表
+    input
+    ------
+    msg: list, 传感器数据
+    traffic: dict, 交通流数据
+    config: dict, 标定参数
+    self.clb: dict, 算法参数
+
+    output
+    ------
+    events: list, 事件列表, 元素为event的衍生类
+    '''
+    events_o = []
+
+    return events_o
+
+
+def incidentSingleCarDetect(msg, traffic, config, clb):
+    '''
+    检测拥堵事件，输出并返回事件列表
+    input
+    ------
+    msg: list, 传感器数据
+    traffic: dict, 交通流数据
+    config: dict, 标定参数
+    self.clb: dict, 算法参数
+
+    output
+    ------
+    events: list, 事件列表, 元素为event的衍生类
+    '''
+    events_is = []
+
+    return events_is
+
+
+def incidentDetect(msg, traffic, config, clb):
+    '''
+    检测拥堵事件，输出并返回事件列表
+    input
+    ------
+    msg: list, 传感器数据
+    traffic: dict, 交通流数据
+    config: dict, 标定参数
+    self.clb: dict, 算法参数
+
+    output
+    ------
+    events: list, 事件列表, 元素为event的衍生类
+    '''
+    events_i = []
+
+    return events_i
+
+
+def intensiveSpeedReductionDetect(msg, traffic, config, clb):
+    '''
+    检测拥堵事件，输出并返回事件列表
+    input
+    ------
+    msg: list, 传感器数据
+    traffic: dict, 交通流数据
+    config: dict, 标定参数
+    self.clb: dict, 算法参数
+
+    output
+    ------
+    events: list, 事件列表, 元素为event的衍生类
+    '''
+    events_r = []
+
+    return events_r
+
+
+
+def lowSpeedDetect(msg, traffic, config, clb):
+    '''
+    检测拥堵事件，输出并返回事件列表
+    input
+    ------
+    msg: list, 传感器数据
+    traffic: dict, 交通流数据
+    config: dict, 标定参数
+    self.clb: dict, 算法参数
+
+    output
+    ------
+    events: list, 事件列表, 元素为event的衍生类
+    '''
+    events_l = []
+
+    return events_l
+
+
+def spillDetect(msg, traffic, config, clb):
+    '''
+    检测拥堵事件，输出并返回事件列表
+    input
+    ------
+    msg: list, 传感器数据
+    traffic: dict, 交通流数据
+    config: dict, 标定参数
+    self.clb: dict, 算法参数
+
+    output
+    ------
+    events: list, 事件列表, 元素为event的衍生类
+    '''
+    events_s = []
+
+    return events_s
+
+
+class event:
+    '''
+    事件基础类。不同类型的具体事件以此为基类。
+    '''
+    def __init__(self, event_name, event_type, event_time, event_location, event_level, event_description):
+        self.event_name = event_name
+        self.event_type = event_type
+        self.event_time = event_time
+        self.event_location = event_location
+        self.event_level = event_level
+        self.event_description = event_description
+        self.event_status = 'new'
+        
