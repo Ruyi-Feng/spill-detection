43e8a4ba8bcd2cb1be9ac6d0328ca9f8bdf4ffd3
Dongyu Tian <2502533535@qq.com>
2023-12-08 18:43:19 +0800

    feat: establish class of lane and cell

---
+++ lane_manager/lane_manager.py
@@ -0,0 +1,33 @@
+
+
+class Cell:
+    '''class Cell
+    实例化道路元胞
+    '''
+    def __init__(self, laneid: int, ) -> None:
+        pass
+
+
+
+
+class Lane:
+    '''class Lane
+    按照车道管理车道属性和交通流参数
+    '''
+    def __init__(self, id: int, emg: bool, len: float, coeff: dict, 
+                 cell: dict):
+        ''' function __init__
+        
+        定义属性：
+        1. 车道编号
+        2. 是否为应急车道
+        3. 车道长度
+        4. 车道线方程（左右中）
+        5. 可以再定义一个子class表示cell，按order索引cell，cell包含： lane, order, vCache, q, k, v, danger
+        '''
+        self.id = id
+        self.emg = emg
+        self.len = len
+        self.coeff = coeff
+        self.cell = cell
+    
