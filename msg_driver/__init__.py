472b8e8e63e0879c3f44829bc31b08d1fdf83dc5
Dongyu Tian <2502533535@qq.com>
2023-12-09 09:25:17 +0800

    feat: add smth property for driver

--- msg_driver/__init__.py
+++ msg_driver/__init__.py
@@ -1,7 +1,8 @@
 class Driver():
     '''class Driver
 
-    数据格式转化驱动器，将传感器数据转化为代码内部流通的数据格式，将代码内部流通的数据格式转化为输出数据。
+    数据格式转化驱动器，将传感器数据转化为代码内部流通的数据格式，
+    将代码内部流通的数据格式转化为输出数据。
     '''
     def receive(self, msg: list) -> list:
         '''function receive
@@ -14,14 +15,19 @@ class Driver():
         ------
         msg: list, 代码内流通的数据格式。
 
-        接受传来的数据message, 将原始数据格式转化为代码内流通的数据格式。返回值与接受的msg相比: 
+        接受传来的数据message, 将原始数据格式转化为代码内流通的数据格式。
+        返回值与接受的msg相比: 
         1. 增加加速度属性a
         2. 增加速度属性speed, 单位m/s
+        3. 增加所在元胞属性cell
+        4. 增加三次指数平滑值属性smth
         '''
         for i in range(len(msg)):
             msg[i]['a'] = 0
             msg[i]['speed'] = 0
             msg[i]['cell'] = {'lane': -1, 'order': -1}
+            msg[i]['smth'] = {'x': {1: 0, 2: 0, 3: 0},
+                              'y': {1: 0, 2: 0, 3: 0}}     # 存储三次指数平滑值
         return msg
 
     def send(self, msg: list) -> list:
@@ -38,9 +44,12 @@ class Driver():
         将代码内部流通的数据, 转化为输出需要的格式。返回值与代码内流通相比相比: 
         1. 删除加速度属性a
         2. 删除速度属性speed
+        3. 删除所在元胞属性cell
+        4. 删除三次指数平滑值属性smth
         '''
         for i in range(len(msg)):
             del msg[i]['a']
             del msg[i]['speed']
             del msg[i]['cell']
+            del msg[i]['smth']
         return msg
