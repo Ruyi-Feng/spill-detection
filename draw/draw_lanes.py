7ee1a288c0b2ab8c730556ffd4593968fc21fbb5
Dongyu Tian <2502533535@qq.com>
2023-12-20 20:28:03 +0800

    fix: update laen draw

--- draw/draw_lanes.py
+++ draw/draw_lanes.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy import array
 import pandas as pd
 
 
@@ -12,16 +13,11 @@ def drawLanes():
     colX = data.columns[xIndex]
     colY = data.columns[yIndex]
     colLane = data.columns[laneIndex]
+    # data的colLane列， 10X车道直接改成X车道轨迹
+    data[colLane] = data[colLane].apply(lambda x: x - 100 if x > 100 else x)
 
     # 车道数据
-    lanePoly = {1: np.array([-0.907055758, -4.51024210, 769.5545122]),
-                2: np.array([ -0.90705576,  -4.5102421 , 245.42938442]),
-                3: np.array([ -0.90705576,  -4.5102421 , 280.04165255]),
-                4: np.array([ -0.90705576,  -4.5102421 , 282.55912225]),
-                5: np.array([ -0.90705576,  -4.5102421 , 536.99610447]),
-                6: np.array([ -0.90705576,  -4.5102421 , 666.7797432 ]),
-                7: np.array([ -0.90705576,  -4.5102421 , 786.30118203]),
-                8: np.array([-0.907055758, -4.51024210, 803.0478517])}
+    lanePoly = {7: array([ -0.90705576,  -4.5102421 , 786.30118203]), 2: array([ -0.90705576,  -0.90705576, 249.84514767]), 3: array([ -0.90705576,  -0.90705576, 273.8357667 ]), 4: array([ -0.90705576,  -0.90705576, 258.48569707]), 5: array([ -0.90705576,  -0.90705576, 493.1350254 ]), 6: array([ -0.90705576,  -0.90705576, 621.0076425 ]), 1: array([ -0.90705576,  -0.90705576, 233.09847791]), 8: array([ -0.90705576,  -4.5102421 , 803.04785179])}
 
     # 画图
     plt.figure(figsize=(16, 16))
@@ -30,11 +26,13 @@ def drawLanes():
     # 画轨迹
     for group, df_group in data.groupby(colLane):
         # 加alpha会变糊
-        plt.scatter(df_group[colX], df_group[colY], label=group, s=1, alpha=0.5)
+        plt.scatter(df_group[colX], df_group[colY], label=group, s=1, alpha=0.2)
     # 画车道
     # lanePoly为每个laneID对应的二次拟合函数，系数为a*x^2+b*x+c
     # 每个车道中心线在一定范围的x内进行采样画图
     xmin, xmax = min(data[colX]), max(data[colX])
+    ymin, ymax = min(data[colY]), max(data[colY])
+    # xmin, xmax = min(data[colX]), 50
     xArr = np.linspace(xmin, xmax, 100)
     for laneID in lanePoly:
         yArr = lanePoly[laneID][0] * xArr * xArr + \
@@ -44,6 +42,7 @@ def drawLanes():
     # 添加元素
     plt.xlabel("x/m")
     plt.ylabel("y/m")
+    plt.ylim(ymin-100, ymax+100)
     plt.title("Trajectory")
     plt.legend(loc="best")
     plt.savefig("./trajectoryWithLane.png", dpi=300)
