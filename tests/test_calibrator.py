b7eee5d4baa7e603ad530e198dd72cb573d9c420
Dongyu Tian <2502533535@qq.com>
2023-12-30 12:27:50 +0800

    fix: add standard result for calib test

--- tests/test_calibrator.py
+++ tests/test_calibrator.py
@@ -1,4 +1,5 @@
 from rsu_simulator import Smltor
+from message_driver import Driver
 from road_calibration import Calibrator
 import yaml
 
@@ -35,15 +36,17 @@ def test_calibrator():
     # 生成仿真器
     dataPath = './data/result.txt'
     smltor = Smltor(dataPath)
+    # 生成驱动器
+    d = Driver()
 
     # 仿真器读取数据
     while True:
         msg = smltor.run()
         if msg == '':
             break
-        if type(msg) == str:    # 非目标信息
+        valid, msg = d.receive(msg)
+        if not valid:
             continue
-
         # 标定器接受数据
         calibrator.run(msg)
 
@@ -51,6 +54,76 @@ def test_calibrator():
     calibrator.calibrate()
 
     # 标定器保存
-    traffic = calibrator.save()
+    clb = calibrator.save()
 
-    assert type(traffic) == dict
+    # 判定
+    standardClb = {
+        1:
+        {
+            'emgc': True, 'vDir': {'x': 1, 'y': -1},
+            'start': 799.3, 'len': 799.3, 'end': 0,
+            'coef': [-0.907, -45.799, 180.391],
+            'cells': [False, False, False, False, False, False, False, False,
+                      False, False, False, False, True, True, True, True]
+        },
+        2:
+        {
+            'emgc': False, 'vDir': {'x': 1, 'y': -1},
+            'start': 799.3, 'len': 799.3, 'end': 0,
+            'coef': [-0.907, -45.799, 197.175],
+            'cells': [False, False, False, False, True, True, True, True,
+                      True, True, True, True, True, True, True, True]
+        },
+        3:
+        {
+            'emgc': False, 'vDir': {'x': 1, 'y': -1},
+            'start': 799.3, 'len': 799.3, 'end': 0,
+            'coef': [-0.907, -35.443, 340.721],
+            'cells': [False, False, False, True, True, True, True, True,
+                      True, True, True, True, True, True, True, True]
+        },
+        4:
+        {
+            'emgc': False, 'vDir': {'x': 1, 'y': -1},
+            'start': 799.3, 'len': 799.3, 'end': 0,
+            'coef': [-0.907, -32.508, 469.615],
+            'cells': [False, False, False, False, False, True, True, True,
+                      True, True, True, True, True, True, True, True]
+        },
+        5:
+        {
+            'emgc': False, 'vDir': {'x': -1, 'y': 1},
+            'start': 0, 'len': 799.3, 'end': 799.3,
+            'coef': [-0.907, -17.134, 715.163],
+            'cells': [False, True, True, True, True, True, True, True,
+                      True, True, True, True, True, True, True, True]
+        },
+        6:
+        {
+            'emgc': False, 'vDir': {'x': -1, 'y': 1},
+            'start': 0, 'len': 799.3, 'end': 799.3,
+            'coef': [-0.907, -12.016, 762.724],
+            'cells': [False, True, True, True, True, True, True, True,
+                      True, True, True, True, True, True, True, True]
+        },
+        7:
+        {
+            'emgc': False, 'vDir': {'x': -1, 'y': 1},
+            'start': 0, 'len': 799.3, 'end': 799.3,
+            'coef': [-0.907, -4.521, 786.292],
+            'cells': [False, True, True, True, True, True, True, True,
+                      True, True, True, True, True, True, True, True]},
+        8:
+        {
+            'emgc': True, 'vDir': {'x': -1, 'y': 1},
+            'start': 0, 'len': 799.3, 'end': 799.3,
+            'coef': [-0.907, -4.521, 803.075],
+            'cells': [False, False, True, False, False, False, True, False,
+                      True, True, True, False, True, True, True, True]
+        }
+    }
+    assert clb == standardClb
+
+
+if __name__ == '__main__':
+    test_calibrator()
