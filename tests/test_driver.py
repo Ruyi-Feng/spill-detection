4c0d79e4e41479f22a4baf4b86afa3897b1b442e
Dongyu Tian <2502533535@qq.com>
2023-12-30 13:37:56 +0800

    fix: add asserts in driver test

--- tests/test_driver.py
+++ tests/test_driver.py
@@ -20,14 +20,25 @@ def test_driver():
         # 模拟接受数据
         # print('接收前', msg[0])
         # 接受数据
-        valid, msg = d.receive(msg)
+        valid, cars = d.receive(msg)
         if not valid:
             continue
         # print('代码内', msg[0])
+        # 检查点1
+        # 成功增加属性
+        if len(cars) > 0:
+            assert cars[0]['a'] == 0
+
         # 发送数据
-        msg = d.send(msg)
+        msg = d.send(cars)
+        # 检查点2
+        # 成功删除属性
+        if len(cars) > 0:
+            assert 'a' not in cars[0].keys()
         # print('发送', msg[0])
         # 断点
         # print('------------------')
-
+    
+    # 检查点3
+    # 数据最后一行为str 
     assert type(msg) == str
