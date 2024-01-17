49fc364e4ae3deff79e713f29a71c9756958fae2
Dongyu Tian <2502533535@qq.com>
2024-01-17 14:16:29 +0800

    fix: update call of event detector in contorller

--- controller.py
+++ controller.py
@@ -3,7 +3,6 @@ import yaml
 from road_calibration import Calibrator
 from message_driver import Driver
 import pre_processing
-from traffic_manager import TrafficMng
 from event_detection import EventDetector
 
 
@@ -12,7 +11,7 @@ class Controller:
 
     properties
     ----------
-    configPath: str
+    cfgPath: str
         算法参数文件路径
     clbPath: str
         标定参数文件路径
@@ -34,39 +33,39 @@ class Controller:
 
     生成控制器，用于控制整个算法流程。
     '''
-    def __init__(self, configPath: str, clbPath: str):
+    def __init__(self, cfgPath: str, clbPath: str):
         '''function __init__
 
         input
         -----
-        configPath: str
+        cfgPath: str
             算法参数文件路径
         clbPath: str
             标定参数文件路径
 
         '''
         # 控制器启动
-        self.configPath = configPath
+        self.cfgPath = cfgPath
         self.clbPath = clbPath
-        # 算法参数
-        config = self._loadyaml(configPath)
-        self.config = config
+        # 配置参数
+        cfg = self._loadyaml(cfgPath)
+        self.cfg = cfg
         # 是否标定
         self.needClb = False
         self.clbtor = None
-        self.calibFrames = config['calib']['calib_seconds'] * config['fps']
+        self.calibFrames = cfg['calib']['calib_seconds'] * cfg['fps']
         self.calibCount = 0
-        if not (os.path.exists(clbPath)) | self.config['calib']['if_recalib']:
+        if not (os.path.exists(clbPath)) | self.cfg['calib']['if_recalib']:
             print('开始标定过程')
-            # 没有config或者配置需要则标定
+            # 没有cfg或者配置需要则标定
             self.needClb = True
-            clbtor = Calibrator(clbPath=clbPath, fps=config['fps'],
-                                laneWidth=config['calib']['laneWidth'],
-                                emgcWidth=config['calib']['emgcWidth'],
-                                cellLen=config['calib']['cell_len'],
-                                qMerge=config['calib']['q_merge'])
+            clbtor = Calibrator(clbPath=clbPath, fps=cfg['fps'],
+                                laneWidth=cfg['calib']['laneWidth'],
+                                emgcWidth=cfg['calib']['emgcWidth'],
+                                cellLen=cfg['calib']['cell_len'],
+                                qMerge=cfg['calib']['q_merge'])
             self.clbtor = clbtor
-        else:   # 有config则读取, 不需要标定
+        else:   # 有cfg则读取, 不需要标定
             print('开始接收数据')
             self.clb = self._loadyaml(clbPath)
             self.startManager()
@@ -83,10 +82,6 @@ class Controller:
         ------
         msg: str | list
             发送数据, str | list格式。str为传输信息(不处理), list为传感器数据。
-        ? traffic: dict
-            交通流参数, dict格式。
-        ? event: dict
-            事件检测结果, dict格式。
 
         接受传感器数据，返回发送数据、交通流参数、事件检测结果。
         根据条件判断是否需要标定，若需要则标定。
@@ -109,23 +104,8 @@ class Controller:
         '''
         # 生成数据驱动器
         self.drv = Driver()
-        # 生成交通管理器
-        self.tm = TrafficMng(self.clb, self.config)
-        # 生成事件检测器
-        self.edt = EventDetector(self.config['fps'],
-                                 self.config['event']['event_types'],
-                                 self.config['event']['v_low'],
-                                 self.config['event']['v_high'],
-                                 self.config['event']['t_tolerance'],
-                                 self.config['event']['q_standard'],
-                                 self.config['event']['rate2'],
-                                 self.config['event']['d_touch'],
-                                 self.config['event']['density_crowd'],
-                                 self.config['event']['v_crowd'],
-                                 self.config['event']['a_intense'],
-                                 self.config['event']['duration_intense'],
-                                 self.config['event']['duration_low'],
-                                 self.config['event']['duration_high'])
+        # 生成事件检测器(内含交通参数管理器)
+        self.edt = EventDetector(self.cfg['fps'], self.clb, self.cfg)
 
     def run(self, msg: list) -> (list, list):
         # 接受数据
@@ -137,10 +117,8 @@ class Controller:
             self.calibration(cars)
         # 预处理
         cars = pre_processing.preProcess(cars, self.trm)
-        # 交通流参数计算
-        traffic = self.tm.run(cars)
-        # 事件检测
-        event = self.edt.run(cars, traffic)
+        # 事件检测(内含交通流参数计算+事件检测)
+        event = self.edt.run(cars)
         # 发送数据
         msg = self.drv.send(cars)
         # print(msg)
@@ -156,9 +134,9 @@ class Controller:
 
         return
         ------
-        config: dict
+        cfg: dict
             配置参数
         '''
         with open(path, 'r') as f:
-            config = yaml.load(f, Loader=yaml.FullLoader)
-        return config
+            cfg = yaml.load(f, Loader=yaml.FullLoader)
+        return cfg
