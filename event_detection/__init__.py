5e96488bf7fd6d7f2c70a1a533159181e7fa9ef6
Dongyu Tian <2502533535@qq.com>
2024-01-17 14:09:08 +0800

    fix: update init of event detector

--- event_detection/__init__.py
+++ event_detection/__init__.py
@@ -1,96 +1,137 @@
 from traffic_manager import TrafficMng
 
 
-default_event_types = ['crowd', 'high_speed', 'illegal_occupation',
-                       'incident_single_car', 'incident',
-                       'intensive_speed_reduction', 'low_speed', 'spill']
+default_event_types = ['spill',
+                       'stop', 'low_speed', 'high_speed', 'emergency_break',
+                       'incident', 'crowd', 'illegal_occupation']
+# 默认配置适用于高速公路
+default_event_config = {'types': default_event_types,
+                        'tt': 300, 'qs': 10000, 'r2': 0.1,
+                        'vs': 2.778, 'ds': 5, 'vl': 11.11, 'dl': 5,
+                        'vh': 33.33, 'dh': 5, 'ai': 3, 'di': 1,
+                        'dt': 5, 'ts': 20, 'dstc': 18, 'vc': 16.67, 'do': 5}
 
 
 class EventDetector(TrafficMng):
     '''class EventDetector
 
+    事件检测类, 用于检测交通事件。继承TrafficMng类, 用于获取交通流信息。
+    唯一对外调用函数: `run(cars)`, 用于更新交通流信息, 检测交通事件, 输出并返回事件列表。
+    此种定义下, 在外围调用时, 无需生成TrafficMng类, 
+    只需生成EventDetector类即可, EventDetector可直接调用TrafficMng的方法。
+
     Properties
     ----------
     fps: float
         frequency per second, 传感器采样频率
-    config: dict
-        检测参数，包括: vl, vh, tt, r2, dt, dstc, vc, ai, di, dl, dh。
-        vl: float, v_low, 低速阈值, 单位m/s
-        vh: float, v_high, 高速阈值, 单位m/s
-        tt: float, t_tolerance, 事件持续时间容忍度, 单位s
-        r2: float, rate2, 抛洒物横向运动置信度增长率
-        dt: float, d_touch, 车辆碰撞判定距离, 单位m
-        dstc: float, density_crowd, 拥堵密度阈值, 单位辆/km
-        vc: float, v_crowd, 拥堵速度阈值, 单位m/s
-        ai: float, a_intense, 急刹车加速度阈值(绝对值), 单位m/s^2
-        di: float, duration_intense, 急刹车持续时间阈值, 单位s
-        dl: float, duration_low, 低速持续时间阈值, 单位s
-        dh: float, duration_high, 高速持续时间阈值, 单位s
     types: list
-        event_types需要检测的事件列表。default=['crowd', 'high_speed',
-        'illegal_occupation', 'incident_single_car', 'incident',
-        'intensive_speed_reduction', 'low_speed', 'spill']
+        event_types需要检测的事件列表, 包括: 抛撒物检测,
+        停车检测, 低速行驶检测, 超速行驶检测, 急停车检测,
+        事故检测, 拥堵检测, 非法占用应急车道检测。
+    
+    检测参数，包括: tt, qs, r2, vs, ds, vl, dl, vh, dh, ai, di,
+                   dt, ts, dstc, vc, do
+    抛洒物检测类
+    tt: float, t_tolerance, 事件持续时间容忍度, 单位s
+    qs: float, q_standard, 标准情况的道路通行流量, 单位v/h
+    r2: float, rate2, 抛洒物横向运动置信度增长率
+    异常行驶类
+    vs: float, v_static, 准静止判定速度阈值, 单位m/s
+    ds: float, duration_static, 准静止持续时间阈值, 单位s
+    vl: float, v_low, 低速阈值, 单位m/s
+    dl: float, duration_low, 低速持续时间阈值, 单位s
+    vh: float, v_high, 高速阈值, 单位m/s
+    dh: float, duration_high, 高速持续时间阈值, 单位s
+    ai: float, a_intense, 急刹车加速度阈值(绝对值), 单位m/s^2
+    di: float, duration_intense, 急刹车持续时间阈值, 单位s
+    车辆碰撞检测类
+    dt: float, d_touch, 车辆碰撞判定距离, 单位m
+    ts: float, t_supervise, 车辆碰撞监控时间, 单位s
+    拥堵检测类
+    dstc: float, density_crowd, 拥堵密度阈值, 单位辆/km
+    vc: float, v_crowd, 拥堵速度阈值, 单位m/s
+    非法占道类
+    do: float, duration_occupation, 非法占道持续时间阈值, 单位s
 
-    事件检测类, 用于检测交通事件。继承TrafficMng类, 用于获取交通流信息。
-    此种定义下, 在外围调用时, 无需生成TrafficMng类, 
-    只需生成EventDetector类即可, EventDetector可直接调用TrafficMng的方法。
     '''
-    def __init__(self, fps, clb, cfg, event_types=default_event_types,
-                 vl: float = 2.778, vh: float = 33.33,
-                 tt: float = 300, qs: float = 10000,
-                 r2: float = 0.1, dt: float = 5,
-                 dstc: float = 18, vc: float = 16.67,
-                 ai: float = 3, di: float = 1,
-                 dl: float = 5, dh: float = 5):
+    def __init__(self, fps, clb, cfg):
         ''' function __init__
 
         input
         ------
         fps: float
             frequency per second, 传感器采样频率
-
+        clb: dict
+            已标定的参数字典
+        cfg: dict
+            配置参数字典
         event_types: list
-            需要检测的事件列表。
-        vl: float
-            v_low, 低速阈值, 单位m/s
-        vh: float
-            v_high, 高速阈值, 单位m/s
-        tt: float
-            t_tolerance, 事件持续时间容忍度, 单位s
-        qs: float
-            q standard, 标准情况的道路通行流量, 单位v/h
-        r2: float
-            rate2, 抛洒物横向运动置信度增长率
-        dt: float
-            d_touch, 车辆碰撞判定距离, 单位m
-        dstc: float
-            density_crowd, 拥堵密度阈值, 单位辆/km
-        vc: float
-            v_crowd, 拥堵速度阈值, 单位m/s
-        ai: float
-            a_intense, 急刹车加速度阈值(绝对值), 单位m/s^2
-        di: float
-            duration_intense, 急刹车持续时间阈值, 单位s
-        dl: float
-            duration_low, 低速持续时间阈值, 单位s
-        dh: float
-            duration_high, 高速持续时间阈值, 单位s
+            需要检测的事件列表
 
         生成事件检测器，用于检测交通事件。
         '''
-        # TODO 将eventdetector的初始化调整成适应接收clg和cfg的形式
-        # TODO 检查tfm和ed是否有相同命名冲突
-        # TODO 重命名config属性为cfg
-        # TODO 将各种检测函数调整为类的方法
         # TODO 在外层要将所有trafficmanager的方法都调整为由eventdetector调用
-        # TODO 为eventdetector设置一个新的run方法，用于更新交通数据和检测事件
+
         # 令TrafficMng方法初始化给自身
         super().__init__(clb, cfg)
+        # 初始化属性(其中秒为单位属性, 统一初次赋值后, 乘fps以帧为单位)
         self.fps = fps
-        self.config = {'vl': vl, 'vh': vh, 'tt': tt, 'qs': qs, 'r1': 1/tt,
-                       'r2': r2, 'dt': dt, 'dstc': dstc, 'vc': vc,
-                       'ai': ai, 'di': di, 'dl': dl, 'dh': dh}
-        self.types = event_types
+        self.types = cfg['event_types'] if 'event_types' in cfg.keys() else \
+            default_event_config['types']
+        # 抛洒物检测参数
+        self.tt = cfg['tt'] if 't_tolerance' in cfg.keys() else \
+            default_event_config['tt']
+        self.qs = cfg['qs'] if 'q_standard' in cfg.keys() else \
+            default_event_config['qs']
+        self.r2 = cfg['r2'] if 'rate2' in cfg.keys() else \
+            default_event_config['r2']
+        # 异常行驶检测参数
+        self.vs = cfg['vs'] if 'v_static' in cfg.keys() else \
+            default_event_config['vs']
+        self.ds = cfg['ds'] if 'duration_static' in cfg.keys() else \
+            default_event_config['ds']
+        self.vl = cfg['vl'] if 'v_low' in cfg.keys() else \
+            default_event_config['vl']
+        self.dl = cfg['dl'] if 'duration_low' in cfg.keys() else \
+            default_event_config['dl']
+        self.vh = cfg['vh'] if 'v_high' in cfg.keys() else \
+            default_event_config['vh']
+        self.dh = cfg['dh'] if 'duration_high' in cfg.keys() else \
+            default_event_config['dh']
+        self.ai = cfg['ai'] if 'a_intense' in cfg.keys() else \
+            default_event_config['ai']
+        self.di = cfg['di'] if 'duration_intense' in cfg.keys() else \
+            default_event_config['di']
+        # 车辆碰撞检测参数
+        self.dt = cfg['dt'] if 'd_touch' in cfg.keys() else \
+            default_event_config['dt']
+        self.ts = cfg['ts'] if 't_supervise' in cfg.keys() else \
+            default_event_config['ts']
+        # 拥堵检测参数
+        self.dstc = cfg['dstc'] if 'density_crowd' in cfg.keys() else \
+            default_event_config['dstc']
+        self.vc = cfg['vc'] if 'v_crowd' in cfg.keys() else \
+            default_event_config['vc']
+        # 非法占道检测参数
+        self.do = cfg['do'] if 'duration_occupation' in cfg.keys() else \
+            default_event_config['do']
+        
+        # 更新参数单位从秒到帧
+        self.tt *= self.fps     # 抛洒物存在持续时间容忍度
+        self.ds *= self.fps     # 准静止持续时间阈值
+        self.dl *= self.fps     # 低速持续时间阈值
+        self.dh *= self.fps     # 高速持续时间阈值
+        self.di *= self.fps     # 急刹车持续时间阈值
+        self.ts *= self.fps     # 车辆碰撞监控时间
+        self.do *= self.fps     # 非法占道持续时间阈值
+
+        # 初始化潜在事件记录变量
+        # 数据格式为: {车辆id: 持续帧数count}
+        self.staticDict = dict()
+        self.lowSpeedDict = dict()
+        self.highSpeedDict = dict()
+        self.intenseDict = dict()
+        self.occupationDict = dict()
 
     def run(self, msg: list) -> list:
         ''' function run
@@ -108,13 +149,22 @@ class EventDetector(TrafficMng):
         更新交通流信息, 检测交通事件, 输出并返回事件列表。
         '''
         # 更新交通流信息
-        self.update(msg)    # TODO 待定
+        self.update(msg)
         # 检测交通事件
+        self.updatePotentialEventDict()
         events = self.detect(msg)
         return events
 
-    def detect(self, msg: list) -> list:
+    def updatePotentialEventDict(self):
+        '''function updatePotentialEventDict
+        更新潜在事件记录变量
         '''
+        # TODO
+        pass
+
+    def detect(self, msg: list) -> list:
+        '''function detect
+
         检测交通事件，输出并返回事件列表
         input
         ------
@@ -131,143 +181,38 @@ class EventDetector(TrafficMng):
 
         # 事件检测
         events = []
-        # 直接数值检测
-        if 'crowd' in self.types:
-            events_c = self.crowdDetect()
-            events += events_c
-        # 群体性检测
-        if 'incident' in self.types:
-            events_i = self.incidentDetect(msg)
-            events += events_i
+        # 抛洒物检测
         if 'spill' in self.types:
-            events_s = self.spillDetect()
+            events_s = self._spillDetect()
             events += events_s
-        # 单体性检测
-        if 'high_speed' in self.types:
-            events_h = self.highSpeedDetect(msg)
-            events += events_h
-        if 'illegal_occupation' in self.types:
-            events_o = self.illegalOccupationDetect(msg)
-            events += events_o
-        if 'incident_single_car' in self.types:
-            events_is = self.incidentSingleCarDetect(msg)
-            events += events_is
-        if 'intensive_speed_reduction' in self.types:
-            events_r = self.SuddenBrakingDetect(msg)
-            events += events_r
+        # 异常行驶检测
+        if 'stop' in self.types:
+            events_s = self._stopDetect(msg)
+            events += events_s
         if 'low_speed' in self.types:
-            events_l = self.lowSpeedDetect(msg)
+            events_l = self._lowSpeedDetect(msg)
             events += events_l
-
+        if 'high_speed' in self.types:
+            events_h = self._highSpeedDetect(msg)
+            events += events_h
+        if 'emergency_break' in self.types:
+            events_r = self._emergencyBrakeDetect(msg)
+            events += events_r
+        # 碰撞事故检测
+        if 'incident' in self.types:
+            events_i = self._incidentDetect(msg)
+            events += events_i
+        # 拥堵检测
+        if 'crowd' in self.types:
+            events_c = self._crowdDetect()
+            events += events_c
+        # 非法占道检测
+        if 'illegal_occupation' in self.types:
+            events_o = self._illegalOccupationDetect(msg)
+            events += events_o
         return events
 
-    def crowdDetect(self) -> list:
-        '''function crowdDetect
-
-        output
-        ------
-        events: list, 事件列表, 元素为event的衍生类
-
-        检测拥堵事件，输出并返回事件列表
-        '''
-        events_c = []
-
-        return events_c
-
-    def highSpeedDetect(self, msg: list) -> list:
-        '''function highSpeedDetect
-
-        input
-        ------
-        msg:
-            list, 传感器数据
-
-        output
-        ------
-        events: list, 事件列表, 元素为event的衍生类
-
-        检测超速事件, 输出并返回事件列表
-        '''
-        events_h = []
-
-        return events_h
-
-    def illegalOccupationDetect(self, msg: list) -> list:
-        '''function illegalOccupationDetect
-
-        input
-        ------
-        msg:
-            list, 传感器数据
-
-        output
-        ------
-        events: list, 事件列表, 元素为event的衍生类
-
-        检测非法占用应急车道事件, 输出并返回事件列表
-        '''
-        events_o = []
-
-        return events_o
-
-    
-    def incidentSingleCarDetect(self, msg: list) -> list:
-        '''function incidentSingleCarDetect
-
-        input
-        ------
-        msg:
-            list, 传感器数据
-
-        output
-        ------
-        events: list, 事件列表, 元素为event的衍生类
-
-        检测单车事故事件, 输出并返回事件列表
-        '''
-        events_is = []
-
-        return events_is
-
-    def incidentDetect(self, msg: list) -> list:
-        '''function incidentDetect
-
-        input
-        ------
-        msg:
-            list, 传感器数据
-
-        output
-        ------
-        events: list, 事件列表, 元素为event的衍生类
-
-        检测多车事故事件, 输出并返回事件列表
-        '''
-        events_i = []
-
-        return events_i
-
-    
-    def lowSpeedDetect(self, msg: list) -> list:
-        '''function lowSpeedDetect
-
-        input
-        ------
-        msg:
-            list, 传感器数据
-
-        output
-        ------
-        events: list, 事件列表, 元素为event的衍生类
-
-        检测低速事件, 输出并返回事件列表
-        '''
-        events_l = []
-
-        return events_l
-
-    
-    def spillDetect(self) -> list:
+    def _spillDetect(self) -> list:
         '''function spillDetect
 
         input
@@ -285,8 +230,61 @@ class EventDetector(TrafficMng):
 
         return events_s
 
-    
-    def SuddenBrakingDetect(self, msg: list) -> list:
+    def _stopDetect(self, msg: list) -> list:
+        '''function stopDetect
+
+        input
+        ------
+        msg:
+            list, 传感器数据
+
+        output
+        ------
+        events: list, 事件列表, 元素为event的衍生类
+
+        检测低速事件, 输出并返回事件列表
+        '''
+        events_l = []
+
+        return events_l
+
+    def _lowSpeedDetect(self, msg: list) -> list:
+        '''function lowSpeedDetect
+
+        input
+        ------
+        msg:
+            list, 传感器数据
+
+        output
+        ------
+        events: list, 事件列表, 元素为event的衍生类
+
+        检测低速事件, 输出并返回事件列表
+        '''
+        events_l = []
+
+        return events_l
+
+    def _highSpeedDetect(self, msg: list) -> list:
+        '''function highSpeedDetect
+
+        input
+        ------
+        msg:
+            list, 传感器数据
+
+        output
+        ------
+        events: list, 事件列表, 元素为event的衍生类
+
+        检测超速事件, 输出并返回事件列表
+        '''
+        events_h = []
+
+        return events_h
+  
+    def _emergencyBrakeDetect(self, msg: list) -> list:
         '''function intensiveSpeedReductionDetect
 
         input
@@ -303,3 +301,52 @@ class EventDetector(TrafficMng):
         events_r = []
 
         return events_r
+
+    def _incidentDetect(self, msg: list) -> list:
+        '''function incidentDetect
+
+        input
+        ------
+        msg:
+            list, 传感器数据
+
+        output
+        ------
+        events: list, 事件列表, 元素为event的衍生类
+
+        检测多车事故事件, 输出并返回事件列表
+        '''
+        events_i = []
+
+        return events_i
+
+    def _crowdDetect(self) -> list:
+        '''function crowdDetect
+
+        output
+        ------
+        events: list, 事件列表, 元素为event的衍生类
+
+        检测拥堵事件，输出并返回事件列表
+        '''
+        events_c = []
+
+        return events_c
+
+    def _illegalOccupationDetect(self, msg: list) -> list:
+        '''function illegalOccupationDetect
+
+        input
+        ------
+        msg:
+            list, 传感器数据
+
+        output
+        ------
+        events: list, 事件列表, 元素为event的衍生类
+
+        检测非法占用应急车道事件, 输出并返回事件列表
+        '''
+        events_o = []
+
+        return events_o
