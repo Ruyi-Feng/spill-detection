30b44281624af3005bfa2dd3017f9778d5bcd115
Dongyu Tian <2502533535@qq.com>
2024-01-17 19:05:39 +0800

    fix: update driver test

--- tests/test_driver.py
+++ tests/test_driver.py
@@ -10,33 +10,38 @@ def test_driver():
     s = Smltor(p)
     d = Driver()
 
+    # 测试数据转化接口
+    interface = {'TargetId': 'id',
+                 'XDecx': 'x',
+                 'YDecy': 'y',
+                 'VDecVx': 'vx',
+                 'VDecVy': 'vy',
+                 'Xsize': 'width',
+                 'Ysize': 'length',
+                 'TargetType': 'class',
+                 'LineNum': 'laneID'
+                 }
+
     while True:
         msg = s.run()
         if msg == '':
             break
-        if type(msg) == str:    # 非目标信息
-            continue
 
         # 模拟接受数据
-        # print('接收前', msg[0])
         valid, cars = d.receive(msg)
         if not valid:
             continue
-        # print('代码内', msg[0])
-        # 检查点1
-        # 成功增加属性
+
+        # 检查点1: 成功增加属性
         if len(cars) > 0:
-            assert cars[0]['a'] == 0
+            for key in interface.keys():
+                assert interface[key] in cars[0].keys()
+                assert key not in cars[0].keys()
 
         # 发送数据
         msg = d.send(cars)
-        # 检查点2
-        # 成功删除属性
+        # 检查点2: 成功删除属性
         if len(cars) > 0:
-            assert 'a' not in cars[0].keys()
-        # print('发送', msg[0])
-        # print('------------------')
-
-    # 检查点3
-    # 数据最后一行为str
-    assert type(msg) == str
+            for key in interface.keys():
+                assert interface[key] not in cars[0].keys()
+                assert key in cars[0].keys()
