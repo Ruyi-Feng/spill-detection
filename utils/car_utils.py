0263570448df59a578245cac563594219bd48b3f
Dongyu Tian <2502533535@qq.com>
2024-01-19 16:09:00 +0800

    feat: add car utils func

---
+++ utils/car_utils.py
@@ -0,0 +1,43 @@
+'''Define common functions for car data processing.'''
+
+
+def getCarFromCars(cars: list, id: int) -> dict:
+    '''function _getCarFromCars
+
+    input
+    ------
+    cars: list, 传感器数据
+    id: int, 车辆id
+
+    output
+    ------
+    car: dict, 单车数据
+
+    从传感器数据中获取指定id的单车数据。
+    '''
+    for car in cars:
+        if car['id'] == id:
+            return car  # 找到指定id的车辆
+    return None     # 未找到指定id的车辆
+
+def getCarBaseInfo(car: dict) -> str:
+    '''function _getCarBaseInfo
+
+    input
+    ------
+    car: dict, 单车数据
+
+    return
+    ------
+    info: str, 单车基本信息字符串
+
+    获取单车基本信息字符串, 用于事件信息输出。
+    基本信息包括x,y,laneID,vx,vy。
+    '''
+    info = f"位置(x,y): {round(car['x'], 0)}m, {round(car['y'], 0)}m, " + \
+        f"车道号: {car['laneID']}, " + \
+        "速度(vx,vy,speed): " + \
+        str(round(car['vx'] * 3.6, 0)) + ", " + \
+        str(round(car['vy'] * 3.6, 0)) + ", " + \
+        str(round(car['speed'] * 3.6, 0)) + "km/h "
+    return info
