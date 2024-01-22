38b46dad37a4c4078687942b45721703abd3bb11
Dongyu Tian <2502533535@qq.com>
2024-01-22 16:19:37 +0800

    fix: move test data into seperated files

--- tests/test_calibrator.py
+++ tests/test_calibrator.py
@@ -2,21 +2,13 @@ from rsu_simulator import Smltor
 from message_driver import Driver
 from road_calibration import Calibrator
 import yaml
-
+from tests.test_data.standardClb import standardClb
 
 # 通过
 def test_calibrator():
     '''test function calibrator
 
     测试标定器。
-
-    测试方法:
-    1. 生成标定器
-    2. 生成仿真器
-    3. 仿真器读取数据
-    4. 标定器接受数据
-    5. 标定器标定
-    6. 标定器保存
     '''
     # 读取配置文件
     cfgPath = './config.yml'
@@ -54,73 +46,7 @@ def test_calibrator():
 
     # 标定器保存
     clb = calibrator.save()
-
-    # 判定
-    standardClb = {
-        1:
-        {
-            'emgc': True, 'vDir': {'x': 1, 'y': -1},
-            'start': 799.3, 'len': 799.3, 'end': 0,
-            'coef': [-0.907, -45.799, 180.391],
-            'cells': [False, False, False, False, False, False, False, False,
-                      False, False, False, False, True, True, True, True]
-        },
-        2:
-        {
-            'emgc': False, 'vDir': {'x': 1, 'y': -1},
-            'start': 799.3, 'len': 799.3, 'end': 0,
-            'coef': [-0.907, -45.799, 197.175],
-            'cells': [False, False, False, False, True, True, True, True,
-                      True, True, True, True, True, True, True, True]
-        },
-        3:
-        {
-            'emgc': False, 'vDir': {'x': 1, 'y': -1},
-            'start': 799.3, 'len': 799.3, 'end': 0,
-            'coef': [-0.907, -35.443, 340.721],
-            'cells': [False, False, False, True, True, True, True, True,
-                      True, True, True, True, True, True, True, True]
-        },
-        4:
-        {
-            'emgc': False, 'vDir': {'x': 1, 'y': -1},
-            'start': 799.3, 'len': 799.3, 'end': 0,
-            'coef': [-0.907, -32.508, 469.615],
-            'cells': [False, False, False, False, False, True, True, True,
-                      True, True, True, True, True, True, True, True]
-        },
-        5:
-        {
-            'emgc': False, 'vDir': {'x': -1, 'y': 1},
-            'start': 0, 'len': 799.3, 'end': 799.3,
-            'coef': [-0.907, -17.134, 715.163],
-            'cells': [False, True, True, True, True, True, True, True,
-                      True, True, True, True, True, True, True, True]
-        },
-        6:
-        {
-            'emgc': False, 'vDir': {'x': -1, 'y': 1},
-            'start': 0, 'len': 799.3, 'end': 799.3,
-            'coef': [-0.907, -12.016, 762.724],
-            'cells': [False, True, True, True, True, True, True, True,
-                      True, True, True, True, True, True, True, True]
-        },
-        7:
-        {
-            'emgc': False, 'vDir': {'x': -1, 'y': 1},
-            'start': 0, 'len': 799.3, 'end': 799.3,
-            'coef': [-0.907, -4.521, 786.292],
-            'cells': [False, True, True, True, True, True, True, True,
-                      True, True, True, True, True, True, True, True]},
-        8:
-        {
-            'emgc': True, 'vDir': {'x': -1, 'y': 1},
-            'start': 0, 'len': 799.3, 'end': 799.3,
-            'coef': [-0.907, -4.521, 803.075],
-            'cells': [False, False, True, False, False, False, True, False,
-                      True, True, True, False, True, True, True, True]
-        }
-    }
+    
     # 检查点1
     # 标定器标定结果与标准结果相同
     assert clb == standardClb
