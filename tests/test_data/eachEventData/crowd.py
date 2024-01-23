9705eb156801eca415507d527b9c31a5e8f0bf45
Dongyu Tian <2502533535@qq.com>
2024-01-23 14:08:41 +0800

    feat: add&update event detect data

---
+++ tests/test_data/eachEventData/crowd.py
@@ -0,0 +1,17 @@
+dataCrowdEvent = {
+    'name': 'crowd',
+    'occured': True,
+    'items':
+    {
+        'G0000000': 
+        {
+            'type': 'crowd',
+            'eventID': 'G0000000',
+            'time': 200,
+            'laneID': 3,
+            'q': 0,
+            'k': 30,
+            'v': 3
+        }
+    }
+}
