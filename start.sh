c61c350c232269f8a6826427a03c9a16a3d3b35b
Dongyu Tian <2502533535@qq.com>
2024-03-06 11:55:18 +0800

    fix: update start.sh with &&

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
-python main.py --deviceId "K79+886" &
-python main.py --deviceId "K71+616" &
-python main.py --deviceId "K78+760" &
-python main.py --deviceId "K69+000" &
-python main.py --deviceId "K69+800" &
-python main.py --deviceId "K75+001" &
-python main.py --deviceId "K76+781" &
-python main.py --deviceId "K80+880" &
-python main.py --deviceId "K81+320" &
-python main.py --deviceId "K73+516" &
+python main.py --deviceId "K79+886" &&
+python main.py --deviceId "K71+616" &&
+python main.py --deviceId "K78+760" &&
+python main.py --deviceId "K69+000" &&
+python main.py --deviceId "K69+800" &&
+python main.py --deviceId "K75+001" &&
+python main.py --deviceId "K76+781" &&
+python main.py --deviceId "K80+880" &&
+python main.py --deviceId "K81+320" &&
+python main.py --deviceId "K73+516"
