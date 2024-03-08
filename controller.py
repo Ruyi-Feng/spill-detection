aa7e5f745c82148b7e9756b0dfe9aaa136a1236f
Dongyu Tian <2502533535@qq.com>
2024-03-08 17:12:13 +0800

    feat: add cache in controller

--- controller.py
+++ controller.py
@@ -16,12 +16,22 @@ class Controller:
 
     properties
     ----------
-    cfgPath: str
-        算法参数文件路径
-    clbPath: str
-        标定参数文件路径
-    dataPath: str
-        传感器数据文件路径, 该参数仅在离线模拟时使用, 用于读取离线数据。
+    cfgPath: str, 算法参数文件路径
+    clbPath: str, 标定参数文件路径
+    dataPath: str, 传感器数据文件路径, 该参数仅在离线模拟时使用, 用于读取离线数据。
+    cfg: dict, 算法参数
+
+    needClb: bool, 是否需要标定
+    calibFrames: int, 标定所需帧数
+    calibCount: int, 当前标定帧数
+    clb: dict, 标定参数
+
+    logger: MyLogger, 日志器
+    drv: Driver, 数据驱动器
+    clbtor: Calibrator, 标定器
+    pp: PreProcessor, 数据预处理器
+    edt: EventDetector, 事件检测器
+    cache: 缓存指定时间的数据
 
     methods
     -------
@@ -111,6 +121,7 @@ class Controller:
                          '开始时刻: {}'.format(
                              time.strftime('%Y-%m-%d %H:%M:%S',
                                            time.localtime())))
+        self._initCache()
         # driver接收当前路段的lanes列表
         self.drv.setLanes(list(self.clb.keys()))
         # 生成数据预处理器
@@ -141,11 +152,55 @@ class Controller:
         if self.needClb:
             self.calibration(cars)
             return None, None
+        # 更新缓存
+        self._updateCache(cars)
         # 预处理
         cars = self.pp.run(cars)
         # 事件检测(内含交通流参数计算+事件检测)
         events = self.edt.run(cars)
         # 发送数据
         msg, events = self.drv.send(cars.copy(), events)
-        # 事件上报(events有多个时，上报多次每次上报一个event)
+        # 保存缓存
+        if len(events) > 0:
+            for event in events:
+                self._saveCache(event['eventID'])
+
         return msg, events
+
+    def _initCache(self):
+        '''function _initCache
+
+        初始化缓存
+        '''
+        self.cacheNum = self.cfg['cacheSeconds'] * self.cfg['fps']
+        self.cache = []
+
+    def _updateCache(self, msg: list):
+        '''function _updateCache
+
+        input
+        -----
+        msg: list, 传感器数据
+
+        更新缓存
+        '''
+        self.cache.append(msg)
+        if len(self.cache) > self.cacheNum:
+            self.cache.pop(0)
+
+    def _saveCache(self, eventID: str):
+        '''function _saveCache
+
+        input
+        -----
+        path: str, 缓存文件路径
+
+        保存缓存
+        '''
+        dir = './logger/eventsCache'
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+        path = f'{dir}/{eventID}.txt'
+        with open(path, 'w') as f:
+            for msg in self.cache:
+                f.write(str(msg) + '\n')
