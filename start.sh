75f95fd9e3b54d4af6899ca867d06832056ec039
Dongyu Tian <2502533535@qq.com>
2024-03-21 17:37:59 +0800

    fix: update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,15 @@
-python main.py --deviceId "K79+886" &&
-python main.py --deviceId "K71+616" &&
-python main.py --deviceId "K78+760" &&
-python main.py --deviceId "K69+000" &&
-python main.py --deviceId "K69+800" &&
-python main.py --deviceId "K75+001" &&
-python main.py --deviceId "K76+781" &&
-python main.py --deviceId "K80+880" &&
-python main.py --deviceId "K81+320" &&
-python main.py --deviceId "K73+516"
+#!/bin/bash
+python main.py --deviceId "K68+366" &
+python main.py --deviceId "K68+400" &
+python main.py --deviceId "K69+000" &
+python main.py --deviceId "K69+800" &
+python main.py --deviceId "K71+616" &
+python main.py --deviceId "K73+516" &
+python main.py --deviceId "K75+001" &
+python main.py --deviceId "K76+781" &
+python main.py --deviceId "K78+760" &
+python main.py --deviceId "K79+886" &
+python main.py --deviceId "K80+850" &
+python main.py --deviceId "K80+880" &
+python main.py --deviceId "K81+320" &
+python main.py --deviceId "K81+866"
