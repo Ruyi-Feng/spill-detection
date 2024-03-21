042b25c4af04b2394e312d4555c68e1da397eef0
Dongyu Tian <2502533535@qq.com>
2024-03-21 15:39:12 +0800

    fix: update logger fileHandler with INFO level

--- logger/__init__.py
+++ logger/__init__.py
@@ -13,7 +13,8 @@ class MyLogger(logging.Logger):
     默认日志级别为logging.INFO, 控制台输出级别为logging.INFO
     文件输出级别为logging.WARNING
     '''
-    def __init__(self, deviceID: str, deviceType: int, level=logging.INFO):
+    def __init__(self, deviceID: str, deviceType: int, level=logging.INFO,
+                 fileLevel=logging.INFO, consoleLevel=logging.INFO):
         '''function __init__
 
         input
@@ -22,6 +23,7 @@ class MyLogger(logging.Logger):
         deviceType: int, 设备类型
         level: int, 日志器级别, 默认为logging.INFO
         '''
+        # logger设置
         loggerName = deviceID + '_' + str(deviceType)
         super().__init__(loggerName, level)
         self.setLevel(level)
@@ -29,10 +31,12 @@ class MyLogger(logging.Logger):
         fmtStr = ' - '.join(['%('+i+')s' for i in fmtStrList])
         formatter = logging.Formatter(fmtStr)
         self.formatter = formatter
+        self.fileLevel = fileLevel
+        self.consoleLevel = consoleLevel
         # 控制台日志
         ch = logging.StreamHandler()
         ch.setFormatter(formatter)
-        ch.setLevel(level)
+        ch.setLevel(consoleLevel)
         self.addHandler(ch)
         # 文件日志
         self._checkLogsDir()    # 判断两层文件夹是否存在
@@ -40,13 +44,13 @@ class MyLogger(logging.Logger):
             f'{datetime.now().strftime("%Y-%m-%d")}.log'
         fh = logging.FileHandler(filePath, encoding='utf-8')
         fh.setFormatter(formatter)
-        fh.setLevel(logging.WARNING)
+        fh.setLevel(fileLevel)
         self.addHandler(fh)
 
-    def newFileLog(self):
-        '''function newFileLog
+    def updateDayLogFile(self):
+        '''function updateDayLogFile
 
-        每天12点, 更新一个文件保存当天的log'''
+        每天12点, 更新一个文件保存新一天的log'''
         # 判断当天的文件是否存在
         self._checkLogsDir()        # 判断两层文件夹是否存在
         filePath = f'./logger/logs/{self.name}/' +\
@@ -60,7 +64,7 @@ class MyLogger(logging.Logger):
         # 文件日志
         fh = logging.FileHandler(filePath, encoding='utf-8')
         fh.setFormatter(self.formatter)
-        fh.setLevel(logging.WARNING)
+        fh.setLevel(self.fileLevel)
         self.addHandler(fh)
         return
 
