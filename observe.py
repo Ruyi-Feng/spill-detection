0735c4e25ee98fd8d9bb4b869e8a432c99495ff3
Dongyu Tian <2502533535@qq.com>
2024-03-26 17:36:05 +0800

    feat: add new func to oberserve data recorded

---
+++ observe.py
@@ -0,0 +1,23 @@
+import os
+import json
+from utils import swapQuotes, unixMilliseconds2Datetime, BigFileReader
+
+
+if __name__ == '__main__':
+    dataDir = r'D:\东南大学\科研\金科\data'
+    for file in os.listdir(dataDir):
+        if ('dump' in file) or ('result' in file) or ('heartbeat' in file):
+            continue
+        if 'index' in file:
+            continue
+        path = dataDir + '/' + file
+        reader = BigFileReader(path)
+        rowNum = reader.rowNumber
+        print(f'{file} has {rowNum} rows.')
+        row0 = reader.getRow(0)
+        row0 = json.loads(swapQuotes(row0))
+        rowEnd = reader.getRow(rowNum - 2)
+        rowEnd = json.loads(swapQuotes(rowEnd))
+        timeStart = unixMilliseconds2Datetime(row0['targets'][0]['timestamp'])
+        timeEnd = unixMilliseconds2Datetime(rowEnd['targets'][0]['timestamp'])
+        print(f'{file} starts at {timeStart}, ends at {timeEnd}.')
