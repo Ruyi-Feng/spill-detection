dbf0a931c905a7c3e4f8a35c7d7858b8c789d0bb
Dongyu Tian <2502533535@qq.com>
2024-03-26 22:51:41 +0800

    feat: add func to read recorded data as df

--- rsu_simulator/dataloader.py
+++ rsu_simulator/dataloader.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 
 
 def loadDataByLine(path: str) -> None:
@@ -89,5 +90,57 @@ def loadDataAsList(path: str) -> list:
     return allData
 
 
+def loadDeviceData2df(dataPath: str, deviceID: str, deviceType: int = 1):
+    '''function loadDeviceData2df
+    
+    input
+    -----
+    dataPath: str, 数据文件路径
+    deviceID: str, 设备ID
+    deviceType: int, 设备类型, 默认为1
+    
+    return
+    ------
+    None
+
+    从数据文件中加载指定设备的数据, 并以DataFrame格式保存。
+    '''
+    # 读取数据
+    deviceData = []
+    with open(dataPath) as f:
+        while True:
+            row = f.readline()
+            if row == '':
+                break
+            row = json.loads(row)
+            condition1 = row['deviceID'] == deviceID
+            condition2 = row['deviceType'] == deviceType
+            if not (condition1 and condition2):
+                continue
+            for target in row['targets']:
+                target['deviceID'] = deviceID
+                target['deviceType'] = deviceType
+                deviceData.append(target)
+    # 保存到DataFrame
+    df = pd.DataFrame(deviceData)
+    print(df)
+    csvPath = dataPath.replace('.txt', f'_{deviceID}_{deviceType}.csv')
+    df.to_csv(csvPath, index=False)
+
+
 if __name__ == "__main__":
-    loadDataAsDict("../data/result.txt")
+    # loadDataAsDict("../data/result.txt")
+    dirPath = "./data/extractedData/"
+    fileNameList = ["2024-03-26 08-20-18_2024-03-26 08-20-42.txt",
+                    "2024-03-26 08-29-51_2024-03-26 08-30-34.txt",
+                    "2024-03-26 09-36-21_2024-03-26 09-41-45.txt",
+                    "2024-03-26 09-41-27_2024-03-26 09-41-57.txt"]
+    deviceIDList = ['K81+320',
+                    'K81+866',
+                    'K78+600',
+                    'K73+516']
+    for fileName, deviceID in zip(fileNameList, deviceIDList):
+        if deviceID != 'K81+866':
+            continue
+        loadDeviceData2df(dirPath + fileName, deviceID)
+    print("数据加载完成.")
