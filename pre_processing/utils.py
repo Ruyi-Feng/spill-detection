52ac4201074116080f993bcbbfb35215cf2336ac
Dongyu Tian <2502533535@qq.com>
2024-03-30 22:37:28 +0800

    fix: update frame func, resolving time delay while combining frames

--- pre_processing/utils.py
+++ pre_processing/utils.py
@@ -1,9 +1,9 @@
 """Define functions to process frame data."""
-
+import time
 
 MaxSecMark = 60000
 # 以下参数根据算法所需数据量确定, 算法最多需要2s的历史数据, 大于2.5s的数据即可删除
-HistoricalInterval = 1600  # 同一id容纳的历史数据时间范围
+HistoricalInterval = 1600  # 同一id最多缓存的历史数据时长, 过老数据将被删除
 UpdateInterval = 1600  # 某一id可容忍的不更新数据的时间范围
 
 
@@ -19,11 +19,13 @@ def frameDelete(contextFrames: dict, lastTimestamp: int) -> None:
     """
     guid_list = list(contextFrames.keys())
     for guid in guid_list:
+        # 删除过老旧数据
         if (
             lastTimestamp - contextFrames[guid][0]["timestamp"]
             > HistoricalInterval
         ):
             del contextFrames[guid][0]
+        # 删除过久没有更新过的guid所有数据
         if (
             len(contextFrames[guid]) == 0
             or lastTimestamp - contextFrames[guid][-1]["timestamp"]
@@ -50,17 +52,23 @@ def framesCombination(
 
     将当前帧数据添加到历史帧数据中, 同时重置时间戳, 保证时间戳恒增。进行原地修改。
     """
+    # time1 = time.time()
+    # 当前帧没有目标, 传来空数据, 直接返回
     if len(currentFrame) == 0:
         return set(), lastTimestamp
-    # 如果历史数据不为空
+    # 如果不为第0帧, 已有历史数据
     if contextFrames:
         latestIdSet = set()
         for guid, objInfo in currentFrame.items():
             # 记录最新帧出现的目标id
             latestIdSet.add(guid)
             objInfo["timestamp"] = objInfo["secMark"]
-            while objInfo["timestamp"] < lastTimestamp:
-                objInfo["timestamp"] += MaxSecMark     # tdy?
+            # while objInfo["timestamp"] < lastTimestamp:
+            #     objInfo["timestamp"] += MaxSecMark
+            # 将while改为直接计算
+            objInfo["timestamp"] += MaxSecMark * (
+                (lastTimestamp - objInfo["timestamp"]) // MaxSecMark + 1
+            )
             contextFrames.setdefault(guid, [])
             if (
                 len(contextFrames[guid])
@@ -72,9 +80,16 @@ def framesCombination(
                 contextFrames[guid].append(objInfo)
             current_secMark = objInfo["timestamp"]
         lastTimestamp = current_secMark
+        # time2 = time.time()
         frameDelete(contextFrames, lastTimestamp)
+        # time3 = time.time()
+        # duration1 = time2 - time1
+        # duration2 = time3 - time2
+        # # 输出ms
+        # print(f"framesCombination duration1: {duration1 * 1000:.3f} ms, \
+        #     duration2: {duration2 * 1000:.3f} ms", end='\r')
         return latestIdSet, lastTimestamp
-    # 如果历史数据为空, 直接添加
+    # 如果历史数据为空, 即第0帧, 直接添加
     latestIdSet = set()
     for guid, objInfo in currentFrame.items():
         lastTimestamp = objInfo["timestamp"] = objInfo["secMark"]
