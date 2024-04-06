162ef2cdfd0b46240e937a162da8485c0c3aff16
Dongyu Tian <2502533535@qq.com>
2024-04-07 00:37:06 +0800

    fix: update test func

--- tests/algorithms/carLocCellTest.py
+++ tests/algorithms/carLocCellTest.py
@@ -1,49 +1,55 @@
 from traffic_manager import LaneMng
+import yaml
 
 
 def testCarLocCell():
+    cfgPath = 'config.yml'
+    with open(cfgPath, 'r', encoding='utf-8') as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
     lm = LaneMng(1, False,
                  len=100, start=0, end=100, vdir=1,
-                 coef={}, cellLen=10, clbCell={})
-    car = {'YDecy': 5}
+                 coef={}, cellLen=10, cellsValid=[True]*10,
+                 cfg=cfg, cacheRet=50)
+    car = {'y': 5}
     order = 0
     o = lm._carLocCell(car)
     assert o == order
 
-    car = {'YDecy': 25}
+    car = {'y': 25}
     order = 2
     o = lm._carLocCell(car)
     assert o == order
 
-    car = {'YDecy': 95}
+    car = {'y': 95}
     order = 9
     o = lm._carLocCell(car)
     assert o == order
 
-    car = {'YDecy': 100}
+    car = {'y': 100}
     order = 10
     o = lm._carLocCell(car)
     assert o == order
 
     lm = LaneMng(1, False,
                  len=100, start=100, end=0, vdir=-1,
-                 coef={}, cellLen=10, clbCell={})
-    car = {'YDecy': 5}
+                 coef={}, cellLen=10, cellsValid=[True]*10,
+                 cfg=cfg, cacheRet=50)
+    car = {'y': 5}
     order = 9
     o = lm._carLocCell(car)
     assert o == order
 
-    car = {'YDecy': 25}
+    car = {'y': 25}
     order = 7
     o = lm._carLocCell(car)
     assert o == order
 
-    car = {'YDecy': 95}
+    car = {'y': 95}
     order = 0
     o = lm._carLocCell(car)
     assert o == order
 
-    car = {'YDecy': 100}
+    car = {'y': 100}
     order = 0
     o = lm._carLocCell(car)
     assert o == order
