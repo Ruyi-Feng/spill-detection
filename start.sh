a0b36833ef1fdefa0d37193c2b019339c0d33b2e
Dongyu Tian <2502533535@qq.com>
2024-04-24 20:42:47 +0800

    fix: update start devices

--- start.sh
+++ start.sh
@@ -7,10 +7,11 @@ python main.py --deviceId "K71+616" &
 python main.py --deviceId "K73+516" &
 python main.py --deviceId "K75+001" &
 python main.py --deviceId "K76+781" &
+python main.py --deviceId "K78+600" &
 python main.py --deviceId "K78+760" &
 python main.py --deviceId "K79+886" &
+python main.py --deviceId "K80+766" &
 python main.py --deviceId "K80+850" &
 python main.py --deviceId "K80+880" &
 python main.py --deviceId "K81+320" &
-python main.py --deviceId "K81+866" &
-python save.py
+python main.py --deviceId "K81+866"
